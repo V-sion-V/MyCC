05ae28ab3896a15e273893e183be809dde5a68c2
V-sion-V <1796469419@qq.com>
2021-10-21 23:54:09 +0800

    2.0

--- Parser.java
+++ Parser.java
@@ -6,103 +6,214 @@ public class Parser {
     public static void main(String[] args) {
         Parser parser = new Parser();
         parser.token = parser.in.nextToken();
-        parser.compUnit();
+        SyntaxTree tree = parser.compUnit();
         System.out.println(parser.out);
         System.exit(0);
     }
 
-    void compUnit() {
+    public SyntaxTree compUnit() { //
         out += "define ";
+        SyntaxTree ret = new SyntaxTree(SyntaxTree.CompUnit);
         if (token.type == Token.INT) {
-            funcDef();
+            ret.push(funcDef());
         } else {
             System.exit(1);
         }
+        return ret;
     }
 
-    void funcDef() {
+    public SyntaxTree funcDef() { //
         out += "dso_local ";
-        if (token.type == Token.INT) {
-            funcType();
-            if (token.type == Token.IDT) {
-                ident();
-                if (token.type == Token.LP) {
-                    out += "(";
-                    token = in.nextToken();
-                    if (token.type == Token.RP) {
-                        out += ") ";
-                        token = in.nextToken();
-                        block();
-                    } else {
-                        System.exit(1);
-                    }
-                } else {
-                    System.exit(1);
-                }
-            } else {
-                System.exit(1);
-            }
-        } else {
-            System.exit(1);
-        }
-    }
-
-    void funcType() {
-        if (token.type == Token.INT) {
-            out += "i32 ";
-            token = in.nextToken();
-        } else {
-            System.exit(1);
-        }
-    }
-
-    void ident() {
+        SyntaxTree ret = new SyntaxTree(SyntaxTree.FuncDef);
+        ret.push(funcType());
         if (token.type == Token.IDT && token.content.equals("main")) {
             out += "@main ";
+            ret.push(new SyntaxTree(token));
+            token = in.nextToken();
+            if (token.type == Token.LP) {
+                out += "(";
+                ret.push(new SyntaxTree(token));
+                token = in.nextToken();
+                if (token.type == Token.RP) {
+                    out += ") ";
+                    ret.push(new SyntaxTree(token));
+                    token = in.nextToken();
+                    ret.push(block());
+                } else {
+                    System.exit(1);
+                }
+            } else {
+                System.exit(1);
+            }
+        } else {
+            System.exit(1);
+        }
+        return ret;
+    }
+
+    public SyntaxTree funcType() { //
+        SyntaxTree ret = new SyntaxTree(SyntaxTree.FuncType);
+        if (token.type == Token.INT) {
+            out += "i32 ";
+            ret.push(new SyntaxTree(token));
             token = in.nextToken();
         } else {
             System.exit(1);
         }
+        return ret;
     }
 
-    void block() {
+    public SyntaxTree block() { //
+        SyntaxTree ret = new SyntaxTree(SyntaxTree.Block);
         if (token.type == Token.LB) {
             out += "{\n";
+            ret.push(new SyntaxTree(token));
             token = in.nextToken();
-            if (token.type == Token.RETURN) {
-                stmt();
-                if (token.type == Token.RB) {
-                    out += "}\n";
-                    token = in.nextToken();
-                } else {
-                    System.exit(1);
-                }
+            ret.push(stmt());
+            if (token.type == Token.RB) {
+                out += "}\n";
+                ret.push(new SyntaxTree(token));
+                token = in.nextToken();
             } else {
                 System.exit(1);
             }
         } else {
             System.exit(1);
         }
+        return ret;
     }
 
-    void stmt() {
+    public SyntaxTree stmt() {
+        SyntaxTree ret = new SyntaxTree(SyntaxTree.Stmt);
         if (token.type == Token.RETURN) {
             out += "ret ";
+            ret.push(new SyntaxTree(token));
             token = in.nextToken();
-            if (token.type == Token.NUM) {
-                out += "i32 " + token.content;
+            ret.push(exp());
+            if (token.type == Token.SEMI) {
+                out += "\n";
+                ret.push(new SyntaxTree(token));
                 token = in.nextToken();
-                if (token.type == Token.SEMI) {
-                    out += "\n";
-                    token = in.nextToken();
-                } else {
-                    System.exit(1);
-                }
             } else {
                 System.exit(1);
             }
         } else {
             System.exit(1);
         }
+        return ret;
+    }
+
+    SyntaxTree exp() {
+        SyntaxTree ret = new SyntaxTree(SyntaxTree.Exp);
+        ret.push(addExp());
+        out += ret.getChild().get(0).value + " ";
+        return ret;
+    }
+
+    SyntaxTree addExp() {
+        SyntaxTree ret = new SyntaxTree(SyntaxTree.AddExp);
+        SyntaxTree temp;
+        temp = mulExp();
+        //calc
+        ret.value = temp.value;
+        //
+        ret.push(temp);
+        while(token.type == Token.PLUS || token.type == Token.MINUS) {
+            ret.push(new SyntaxTree(token));
+            //calc
+            if(token.type == Token.PLUS) {
+                //
+                token = in.nextToken();
+                temp = mulExp();
+                //calc
+                ret.value += temp.value;
+            } else {
+                //
+                token = in.nextToken();
+                temp = mulExp();
+                //calc
+                ret.value -= temp.value;
+            }
+            //
+            ret.push(temp);
+        }
+        return ret;
+    }
+
+    SyntaxTree mulExp() {
+        SyntaxTree ret = new SyntaxTree(SyntaxTree.MulExp);
+        SyntaxTree temp;
+        temp = unaryExp();
+        //calc
+        ret.value = temp.value;
+        //
+        ret.push(temp);
+        while(token.type == Token.MULT || token.type == Token.DIV || token.type == Token.MODE) {
+            ret.push(new SyntaxTree(token));
+            //calc
+            if(token.type == Token.MULT) {
+                //
+                token = in.nextToken();
+                temp = unaryExp();
+                //calc
+                ret.value *= temp.value;
+            } else if(token.type == Token.DIV) {
+                //
+                token = in.nextToken();
+                temp = unaryExp();
+                //calc
+                ret.value /= temp.value;
+            } else {
+                //
+                token = in.nextToken();
+                temp = unaryExp();
+                //calc
+                ret.value %= temp.value;
+            }
+            //
+            ret.push(temp);
+        }
+        return ret;
+    }
+
+    SyntaxTree unaryExp() {
+        SyntaxTree ret = new SyntaxTree(SyntaxTree.UnaryExp);
+        while(token.type == Token.PLUS || token.type == Token.MINUS) {
+            ret.push(new SyntaxTree(token));
+            token = in.nextToken();
+        }
+        ret.push(primaryExp());
+        //calc
+        for(SyntaxTree i : ret.getChild()) ret.value *= i.value;
+        //
+        return ret;
+    }
+
+    SyntaxTree primaryExp() {
+        SyntaxTree ret = new SyntaxTree(SyntaxTree.PrimaryExp);
+        if(token.type == Token.NUM) {
+            ret.push(new SyntaxTree(token));
+            token = in.nextToken();
+            //cal
+            ret.value = ret.getChild().get(0).value;
+            //
+        } else if (token.type == Token.LP) {
+            ret.push(new SyntaxTree(token));
+            token = in.nextToken();
+            ret.push(addExp());
+            //cal
+            ret.value = ret.getChild().get(1).value;
+            //
+            if(token.type == Token.RP) {
+                ret.push(new SyntaxTree(token));
+                token = in.nextToken();
+            } else {
+                System.exit(1);
+            }
+        } else {
+            System.exit(1);
+        }
+
+        return ret;
     }
 }
---
+++ SyntaxTree.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class SyntaxTree extends Token {
+    static final int CompUnit = 128, Decl = 129, ConstDecl = 130, BType = 131, ConstDef = 132,ConstInitVal = 133,
+    VarDecl = 134, VarDef = 135, InitVal = 136, FuncDef = 137, FuncType = 138, FuncFParams = 139, FuncFParam = 140,
+    Block = 141, BlockItem = 142, Stmt = 143, Exp = 144, Cond = 145, LVal = 146, PrimaryExp = 147,UnaryExp = 148,
+    UnaryOp = 149, FuncRParams = 150, MulExp = 151, AddExp = 152, RelExp = 153, EqExp = 154, LAndExp = 155,
+    LOrExp = 156, ConstExp = 157;
+    private ArrayList<SyntaxTree> child = null;
+    public int value = 0;
+
+    SyntaxTree(int type) {
+        super(type);
+        setValue();
+    }
+
+    SyntaxTree(Token token) {
+        super(token.type,token.content);
+        setValue();
+    }
+
+    private void setValue(){
+        if(type == MulExp || type == UnaryExp || type == PLUS) value = 1;
+        else if(type == MINUS) value = -1;
+        else if(type == NUM) value = Integer.parseInt(content);
+    }
+
+    public void push(SyntaxTree a) {
+        if(child == null) {
+            child = new ArrayList<>();
+        }
+        child.add(a);
+    }
+
+    public ArrayList<SyntaxTree> getChild() {
+        if(child == null) {
+            child = new ArrayList<>();
+        }
+        return child;
+    }
+
+    public ArrayList<Token> getVt() {
+        ArrayList<Token> ret = new ArrayList<>();
+        if(this.type < 128) {
+            ret.add(this);
+        } else {
+            for(SyntaxTree i : getChild()) {
+                ret.addAll(i.getVt());
+            }
+        }
+        return ret;
+    }
+}
